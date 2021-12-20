a0e23479af496f9ac931cb0be76ef5d9a7c36dc5
TomixChiu <qhr2001@yahoo.com>
2021-12-20 21:28:23 +0800

    update src

---
+++ src/IO/Path.php
@@ -0,0 +1,80 @@
+<?php
+namespace Yauphp\Common\IO;
+
+/**
+ * 路径相关
+ * @author Administrator
+ *
+ */
+class Path
+{
+    /**
+     * 获取文件目录名
+     * @param string $path
+     * @return mixed
+     */
+    public static function getDirName($path)
+    {
+        return pathinfo($path,PATHINFO_DIRNAME);
+    }
+
+    /**
+     * 取得文件名
+     * @param string $path
+     * @return string
+     */
+    public static function getFileBaseName($path)
+    {
+        return pathinfo($path,PATHINFO_BASENAME);
+    }
+
+    /**
+     * 取得文件名,不包括扩展名
+     * @param string $path
+     * @return string
+     */
+    public static function getFileBaseNameWithoutExt($path)
+    {
+        $baseName = pathinfo($path,PATHINFO_BASENAME);
+        $extName= self::getFileExtName($path);
+        $pos=strrpos($baseName, $extName);
+        if($pos>0){
+            $baseName=substr($baseName, 0,$pos-1);
+        }
+        return $baseName;
+    }
+
+    /**
+     * 取得文件扩展名
+     * @param string $path
+     * @return string
+     */
+    public static function getFileExtName($path)
+    {
+        return pathinfo($path,PATHINFO_EXTENSION);
+    }
+
+    /**
+     * 连接路径字符串
+     * @param string[] $paths
+     * @return string
+     */
+    public static function combinePath(...$paths)
+    {
+        //$paths=func_get_args();
+        $value="";
+        for($i=0;$i<count($paths);$i++){
+            $path=$paths[$i];
+            if($i>0){
+                $value.="/";
+                $path=ltrim($path,"/");
+            }
+            if($i<count($paths)-1){
+                $path=rtrim($path,"/");
+            }
+            $value.=$path;
+        }
+        return $value;
+    }
+}
+
