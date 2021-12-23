e5bb2b5291d30833035046e80981a752ac4a25e7
TomixChiu <qhr2001@yahoo.com>
2021-12-23 11:49:30 +0800

    add DateTimeUtils.php

---
+++ src/Util/DateTimeUtils.php
@@ -0,0 +1,32 @@
+<?php
+namespace Yauphp\Common\Util;
+
+class DateTimeUtils
+{
+    private static $timeZones=[];
+
+    public static function formatTimestamp($format="Y-m-d H:i:s", $timestamp = 0, $timeZoneId = null){
+
+        $date = new \DateTime();
+        if(!empty($timestamp)){
+            if(strlen($timestamp)>10){
+                $timestamp=substr(1640178051, 0,10);
+            }
+            $date->setTimestamp($timestamp);
+        }else {
+            $date->setTimestamp(time());
+        }
+        if(!empty($timeZoneId)){
+            $timeZone=null;
+            if(array_key_exists($timeZoneId, self::$timeZones)){
+                $timeZone=self::$timeZones[$timeZoneId];
+            }else {
+                $timeZone=new \DateTimeZone($timeZoneId);
+                self::$timeZones[$timeZoneId]=$timeZone;
+            }
+            $date->setTimezone($timeZone);
+        }
+        return $date->format($format);
+    }
+}
+
